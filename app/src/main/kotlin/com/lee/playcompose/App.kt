b1032ee15f11ed0c2a7f27bb0319bd405d3520fb
lijiawei11 <lijiawei11@baidu.com>
2022-06-24 10:07:47 +0800

    update app init code.

--- app/src/main/kotlin/com/lee/playcompose/App.kt
+++ app/src/main/kotlin/com/lee/playcompose/App.kt
@@ -1,19 +1,20 @@
 package com.lee.playcompose
 
 import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
 import com.lee.playcompose.base.cache.CacheManager
 import com.lee.playcompose.base.core.BaseApplication
 import com.lee.playcompose.base.interadp.SimpleActivityLifecycleCallbacks
 import com.lee.playcompose.base.net.HttpManager
 import com.lee.playcompose.base.tools.DarkModeTools
-import com.lee.playcompose.base.tools.StatusTools.setDarkStatusIcon
-import com.lee.playcompose.base.tools.StatusTools.setLightStatusIcon
-import com.lee.playcompose.base.tools.StatusTools.setNavigationBarColor
+import com.lee.playcompose.common.extensions.appThemeSet
+import com.lee.playcompose.common.extensions.runInternalBlock
 import com.lee.playcompose.common.extensions.setCommonInterceptor
 import com.lee.playcompose.common.paging.db.RemoteCacheDatabase
 import com.lee.playcompose.service.helper.ApplicationModuleService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * 程序主入口
@@ -26,33 +27,29 @@ class App : BaseApplication() {
     private val activityLifecycleCallbacks = object : SimpleActivityLifecycleCallbacks() {
 
         override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
-            // 夜间模式主题适配
-            if (DarkModeTools.get().isDarkTheme()) {
-                activity.setNavigationBarColor(Color.BLACK)
-                activity.setLightStatusIcon()
-            } else {
-                activity.setNavigationBarColor(Color.WHITE)
-                activity.setDarkStatusIcon()
-            }
+            // 应用内夜间模式主题适配
+            activity.runInternalBlock { activity.appThemeSet() }
             super.onActivityCreated(activity, bundle)
         }
 
     }
 
     override fun init() {
-        // 初始化深色模式工具
-        DarkModeTools.init(this)
-        // 缓存管理工具初始化
-        CacheManager.init(this, BuildConfig.VERSION_CODE)
-        // 初始化网络拦截器
-        HttpManager.instance.setCommonInterceptor()
-        // 初始化远程缓存数据库
-        RemoteCacheDatabase.getInstance(this)
-        // 子模块统一初始化
-        ApplicationModuleService.init(this)
+        CoroutineScope(Dispatchers.IO).launch {
+            // 初始化深色模式工具
+            DarkModeTools.init(applicationContext)
+            // 缓存管理工具初始化
+            CacheManager.init(applicationContext, BuildConfig.VERSION_CODE)
+            // 初始化网络拦截器
+            HttpManager.instance.setCommonInterceptor()
+            // 初始化远程缓存数据库
+            RemoteCacheDatabase.getInstance(applicationContext)
+            // 子模块统一初始化
+            ApplicationModuleService.init(this@App)
 
-        // 注册Activity生命周期监听
-        registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
+            // 注册Activity生命周期监听
+            registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
+        }
     }
 
     override fun unInit() {
