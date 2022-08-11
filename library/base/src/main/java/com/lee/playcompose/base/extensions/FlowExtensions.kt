da7054ef245d48787c6931aad694b7dfe401ad08
lijiawei11 <lijiawei11@baidu.com>
2022-08-11 19:45:22 +0800

    update FlowExtensions.kt add lowestTime function.

--- library/base/src/main/java/com/lee/playcompose/base/extensions/FlowExtensions.kt
+++ library/base/src/main/java/com/lee/playcompose/base/extensions/FlowExtensions.kt
@@ -7,8 +7,29 @@ package com.lee.playcompose.base.extensions
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
-fun <T> Flow<T>.delay(timeMillis: Long) = map {
+/**
+ * flow 添加延时回调
+ */
+fun <T> Flow<T>.delay(timeMillis: Long = 300) = map {
     kotlinx.coroutines.delay(timeMillis)
     it
+}
+
+/**
+ * flow 添加最小执行时间回调 （如果从执行到collect未满足该时间则进行延时）
+ */
+fun <T> Flow<T>.lowestTime(timeMillis: Long = 300): Flow<T> {
+    var startTime: Long = 0
+    return onStart {
+        startTime = System.currentTimeMillis()
+    }.map {
+        val currentTime = System.currentTimeMillis()
+        val executionTime = (currentTime - startTime)
+        if (executionTime < timeMillis) {
+            kotlinx.coroutines.delay(timeMillis - executionTime)
+        }
+        it
+    }
 }
