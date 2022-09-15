40657ac7a7f75f212c223d197eb73eac251d326a
lijiawei11 <lijiawei11@baidu.com>
2022-09-15 10:18:36 +0800

    update ContextExtensions.kt

--- library/base/src/main/java/com/lee/playcompose/base/extensions/ContextExtensions.kt
+++ library/base/src/main/java/com/lee/playcompose/base/extensions/ContextExtensions.kt
@@ -7,12 +7,14 @@ package com.lee.playcompose.base.extensions
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.graphics.Point
 import android.os.Build
 import android.util.DisplayMetrics
 import android.util.TypedValue
 import android.view.WindowManager
+import androidx.annotation.AttrRes
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import com.lee.playcompose.base.core.ApplicationExtensions.app
@@ -191,7 +193,7 @@ fun Context.applyDimension(value: Float, unit: Int): Float {
  * @param attrId 属性值
  */
 fun Context.getThemeColor(
-    attrId: Int,
+    @AttrRes attrId: Int,
     @ColorRes defaultId: Int = android.R.color.transparent
 ): Int {
     return theme.obtainStyledAttributes(intArrayOf(attrId))
@@ -203,9 +205,40 @@ fun Context.getThemeColor(
  * @param attrId 属性值
  */
 fun Context.getThemeDimension(
-    attrId: Int,
+    @AttrRes attrId: Int,
     defaultValue: Float = 0F
 ): Float {
     return theme.obtainStyledAttributes(intArrayOf(attrId))
         .getDimension(0, defaultValue)
+}
+
+/**
+ * 获取颜色stateList
+ * @param color 颜色取值resourceID
+ */
+fun Context.getColorStateListCompat(@ColorRes color: Int): ColorStateList {
+    val colorResource = ContextCompat.getColor(this, color)
+    val colors = intArrayOf(colorResource, colorResource)
+    val states = arrayOfNulls<IntArray>(2)
+    states[0] = intArrayOf(android.R.attr.state_pressed)
+    states[1] = intArrayOf()
+    return ColorStateList(states, colors)
+}
+
+/**
+ * 获取颜色stateList
+ * @param checkColor 选中颜色resourceID
+ * @param color 默认颜色resourceID
+ */
+fun Context.getCheckedColorStateListCompat(
+    @ColorRes checkColor: Int,
+    @ColorRes color: Int
+): ColorStateList {
+    val checkColorResource = ContextCompat.getColor(this, checkColor)
+    val colorResource = ContextCompat.getColor(this, color)
+    val colors = intArrayOf(checkColorResource, colorResource)
+    val states = arrayOfNulls<IntArray>(2)
+    states[0] = intArrayOf(android.R.attr.state_checked)
+    states[1] = intArrayOf()
+    return ColorStateList(states, colors)
 }
