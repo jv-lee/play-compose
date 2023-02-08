559eb0090f107b4bfcdaa40536abde5d1c2a057e
lijiawei11 <lijiawei11@baidu.com>
2023-02-08 10:31:25 +0800

    update app init code.

--- app/src/main/kotlin/com/lee/playcompose/App.kt
+++ app/src/main/kotlin/com/lee/playcompose/App.kt
@@ -4,7 +4,6 @@ import com.lee.playcompose.base.core.BaseApplication
 
 /**
  * 程序主入口
- * 进行模块、工具类、基础配置初始化 Activity生命周期统一监听功能业务处理
  * @author jv.lee
  * @date 2022/2/24
  */
@@ -13,5 +12,6 @@ class App : BaseApplication() {
     override fun init() {
     }
 
-    override fun unInit() {}
+    override fun unInit() {
+    }
 }
--- app/src/main/kotlin/com/lee/playcompose/startup/AutoInitializer.kt
+++ app/src/main/kotlin/com/lee/playcompose/startup/AutoInitializer.kt
@@ -9,25 +9,30 @@ import com.lee.playcompose.base.tools.DarkModeTools
 import com.lee.playcompose.common.extensions.setCommonInterceptor
 import com.lee.playcompose.common.paging.db.RemoteCacheDatabase
 import com.lee.playcompose.service.helper.ApplicationModuleService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
 
 /**
- * 项目自动初始化
+ * 项目自动初始化 进行模块、工具类、基础配置初始化
  * @author jv.lee
  * @date 2023/2/7
  */
 class AutoInitializer : Initializer<Unit> {
     override fun create(context: Context) {
-        // 初始化深色模式工具
-        DarkModeTools.init(context)
-        // 缓存管理工具初始化
-        CacheManager.init(context, BuildConfig.VERSION_CODE)
-        // 初始化网络拦截器
-        HttpManager.instance.setCommonInterceptor()
-        // 初始化远程缓存数据库
-        RemoteCacheDatabase.getInstance(context)
-        // 子模块统一初始化
-        ApplicationModuleService.init(context)
+        CoroutineScope(Dispatchers.IO).launch {
+            // 初始化深色模式工具
+            DarkModeTools.init(context)
+            // 缓存管理工具初始化
+            CacheManager.init(context, BuildConfig.VERSION_CODE)
+            // 初始化网络拦截器
+            HttpManager.instance.setCommonInterceptor()
+            // 初始化远程缓存数据库
+            RemoteCacheDatabase.getInstance(context)
+            // 子模块统一初始化
+            ApplicationModuleService.init(context)
+        }
     }
 
     override fun dependencies(): MutableList<Class<out Initializer<*>>> = Collections.emptyList()
